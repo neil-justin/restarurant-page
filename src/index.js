20d38ded808fd5e9b0064c33f31b624d5dc48185
neil <mallarineil9@gmail.com>
2022-10-24 00:16:47 -0400

    Append an event listener to the parent of the main navigation link
    Reason: to  make use of event delegation rather than tediously appending an event listener to each navigation link

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 import displayHomeTabContents from "./tabs/home";
 
-displayHomeTabContents();
+let currentTab = displayHomeTabContents();
+
+function clearTabContents(mainTag) {
+    if (currentTab === 'Home') {
+        /* Setting the attribute's value into 'no' removes the background
+        image of this element and toggles a selector in main.css file that
+        adds a padding into this element */
+        mainTag.setAttribute('data-has-background-image', 'no');
+    } else {
+        /* A method that removes the element's children if no parameter is
+        specified. It was said to be a faster approach of removing element's
+        children hence I go with it. */
+        mainTag.replaceChildren()
+    }
+}
+
+const tablist = document.querySelector('#tablist');
+tablist.addEventListener('click', (e) => {
+    const main = document.querySelector('main');
+
+    if (e.target.textContent !== currentTab) {
+        clearTabContents(main);
+
+        switch (e.target.textContent) {
+            case 'Home':
+                console.log(e.target.textContent);
+                break;
+            case 'Menu':
+                console.log(e.target.textContent);
+                break;
+            case 'Contact':
+                console.log(e.target.textContent);
+        }
+    }
+})
--- src/tabs/home.js
+++ src/tabs/home.js
@@ -1,6 +1,9 @@
 export default function displayHomeTabContents() {
     const main = document.querySelector('main');
+    
     /* add this attribute in order to enable the property of the
     main[data-has-background-image='yes'] selector in '../styles/main.css' */
     main.setAttribute('data-has-background-image', 'yes');
+
+    return 'Home';
 }
