9a69117f60900a31779fd33128a6f0f44a6a9160
neil <mallarineil9@gmail.com>
2022-10-26 10:16:48 -0400

    Fix bug
    Fix the bug wherein the main element is being empty when a click on the empty space inside the parent of the main navigation links is detected

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# restarurant-page
+# Restaurant Page
+A simple restaurant page managed using webpack.
+
+## Preview
+See the restaurant page by clicking [this](https://neil-justin.github.io/restarurant-page/)
+
+
+![Restaurant Page]()
+
+## Objective
+In this project, we are tasked to create our very own restaurant page in order to familiarize ourselves with the workaround of webpack. Rather than manually taking care of the dependencies, we have to automate the file and assets management with the help of npm and webpack. We are also enforced to make use of ES6 modules to maximize the maintainability of the codebase.
+
+## Acknnowledgment
+This project was made possible by [The Odin Project](theodinproject.com) — a community-supported, open-source website that teaches full-stack web development for free.
--- src/index.js
+++ src/index.js
@@ -7,10 +7,10 @@ const mainElem = document.querySelector('main');
 let currentTab = displayHomeTabContents(mainElem);
 
 function clearTabContents(openTab) {
-        /* Setting the attribute's value removes the background
-        image of <main> element when the opened tab is the Home tab. */
-        mainElem.setAttribute('data-tab', openTab);
-        
+    /* Setting the attribute's value removes the background
+    image of <main> element when the opened tab is the Home tab. */
+    mainElem.setAttribute('data-tab', openTab);
+
     if (currentTab !== 'Home') {
         /* A method that removes the element's children if no parameter is
         specified. It was said to be a faster approach of removing element's
@@ -22,19 +22,30 @@ function clearTabContents(openTab) {
 const tablist = document.querySelector('#tablist');
 tablist.addEventListener('click', (e) => {
     if (e.target.textContent !== currentTab) {
-        clearTabContents(e.target.textContent);
+        /* Since I add the event listener not on each individual navigation link,
+        rather, on their parent (to make use of event delegation), the event
+        listener reacts to a click inside the blank area of the parent elemnt
+        which in turns bypass the conditional statement above and produce an
+        unexpected ui result. In order to prevent that, the conditional statement
+        below is needed */
+        if (e.target.textContent === 'Home' ||
+            e.target.textContent === 'Menu' ||
+            e.target.textContent === 'Contact') {
 
-        switch (e.target.textContent) {
-            case 'Home':
-                currentTab = displayHomeTabContents(mainElem);
-                break;
-            case 'Menu':
-                currentTab = displayMenuTabContents(mainElem);
-                break;
-            case 'Contact':
-                currentTab = displayContactTabContents(mainElem);
+            clearTabContents(e.target.textContent);
+
+            switch (e.target.textContent) {
+                case 'Home':
+                    currentTab = displayHomeTabContents(mainElem);
+                    break;
+                case 'Menu':
+                    currentTab = displayMenuTabContents(mainElem);
+                    break;
+                case 'Contact':
+                    currentTab = displayContactTabContents(mainElem);
+            }
+
+            console.log(currentTab);
         }
-
-        console.log(currentTab);
     }
 })
